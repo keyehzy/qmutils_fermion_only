6034141222f8c64646c511206de5ad3259529321
msousa <msousa@sjtu.edu.cn>
2025-01-13 14:00:30 +0800

    refactor(lru_cache): modify lru cache logic to avoid pointers

--- src/qmutils/lru_cache.h
+++ src/qmutils/lru_cache.h
@@ -12,36 +12,34 @@ class LRUCache {
  public:
   using key_type = Key;
   using value_type = Value;
-  using list_type = std::list<std::pair<const Key*, Value>>;
+
+  using list_type = std::list<std::pair<Key, Value>>;
   using map_type = std::unordered_map<Key, typename list_type::iterator>;
 
-  explicit LRUCache(size_t capacity)
-      : capacity_(capacity), cache_map_(std::max(capacity, size_t{16})) {
-    cache_map_.reserve(capacity);
+  explicit LRUCache(size_t capacity) : capacity_(capacity) {
+    cache_map_.reserve(std::max<size_t>(64, capacity));
   }
 
-  void put(const key_type& key, value_type&& value) {
+  template <typename K, typename V>
+  void put(K&& key, V&& value) {
     auto it = cache_map_.find(key);
     if (it != cache_map_.end()) {
-      it->second->second = std::move(value);
+      it->second->second = std::forward<V>(value);
       cache_list_.splice(cache_list_.begin(), cache_list_, it->second);
       return;
     }
 
+    if (capacity_ == 0) {
+      return;
+    }
     if (cache_list_.size() >= capacity_) {
-      const auto& last_key = *cache_list_.back().first;
-      cache_map_.erase(last_key);
+      auto& least_recent_pair = cache_list_.back();
+      cache_map_.erase(least_recent_pair.first);
       cache_list_.pop_back();
     }
 
-    cache_list_.emplace_front(
-        &cache_map_.emplace(key, typename list_type::iterator{}).first->first,
-        std::move(value));
-    cache_map_[key] = cache_list_.begin();
-  }
-
-  void put(const key_type& key, const value_type& value) {
-    put(std::move(key), value_type(value));
+    cache_list_.emplace_front(std::forward<K>(key), std::forward<V>(value));
+    cache_map_[cache_list_.front().first] = cache_list_.begin();
   }
 
   std::optional<std::reference_wrapper<const value_type>> get(
@@ -54,12 +52,23 @@ class LRUCache {
     return std::cref(it->second->second);
   }
 
+  std::optional<std::reference_wrapper<value_type>> get_mutable(
+      const key_type& key) {
+    auto it = cache_map_.find(key);
+    if (it == cache_map_.end()) {
+      return std::nullopt;
+    }
+    cache_list_.splice(cache_list_.begin(), cache_list_, it->second);
+    return std::ref(it->second->second);
+  }
+
   void clear() noexcept {
     cache_map_.clear();
     cache_list_.clear();
   }
 
   size_t size() const noexcept { return cache_map_.size(); }
+
   size_t capacity() const noexcept { return capacity_; }
 
  private:
