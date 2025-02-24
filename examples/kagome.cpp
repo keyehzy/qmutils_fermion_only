7e0471643ebff800655934329c8aeae7c8a55e76
Matheus <msousa@sjtu.edu.cn>
2025-02-24 13:22:47 +0800

    example(Kagome): Add current calculation

--- examples/kagome.cpp
+++ examples/kagome.cpp
@@ -15,10 +15,14 @@ using namespace qmutils;
 
 struct Vec2 {
   float x, y;
-
   Vec2 operator+(const Vec2& other) { return Vec2{x + other.x, y + other.y}; }
-
   Vec2 operator*(float a) { return Vec2{x * a, y * a}; }
+};
+
+struct Bond {
+  Vec2 initial_position;
+  Vec2 final_position;
+  Expression op;
 };
 
 template <size_t Lx, size_t Ly>
@@ -32,9 +36,7 @@ class KagomeModel {
 
   const Expression& hamiltonian() const { return m_hamiltonian; }
   const std::vector<Vec2>& position() const { return m_position; }
-  Vec2 position_of(size_t i, size_t j, size_t k) const {
-    return m_position[m_index.to_orbital(i, j, k)];
-  }
+  const std::vector<Bond> bonds() const { return m_bonds; }
   const Index& index() const { return m_index; }
 
  private:
@@ -42,6 +44,8 @@ class KagomeModel {
   Index m_index;
   Expression m_hamiltonian;
   std::vector<Vec2> m_position;
+  std::vector<Bond> m_bonds;
+  NormalOrderer orderer;
 
   Term density_density_interaction(size_t site) const {
     auto s = Operator::Spin::Up;
@@ -92,6 +96,42 @@ class KagomeModel {
             Vec2{0.25f, 0.25f * std::sqrt(3.0f)} + displacement;
       }
     }
+
+    // Build bonds
+    for (size_t i = 0; i < Lx; ++i) {
+      for (size_t j = 0; j < Ly; ++j) {
+        size_t A_site = m_index.to_orbital(i, j, 0);
+        size_t B_site = m_index.to_orbital(i, j, 1);
+        size_t C_site = m_index.to_orbital(i, j, 2);
+
+        size_t next_A_y_site = m_index.to_orbital(i, (j + 1) % Ly, 0);
+        size_t next_A_x_site = m_index.to_orbital((i + 1) % Lx, j, 0);
+        size_t next_C_x_y_site =
+            m_index.to_orbital((i + 1) % Lx, (j + Ly - 1) % Ly, 2);
+
+        m_bonds.push_back({m_position[A_site], m_position[B_site],
+                           m_t1 * edge(A_site, B_site, s)});
+        m_bonds.push_back({m_position[B_site], m_position[C_site],
+                           m_t1 * edge(B_site, C_site, s)});
+        m_bonds.push_back({m_position[C_site], m_position[A_site],
+                           m_t1 * edge(C_site, A_site, s)});
+
+        m_bonds.push_back({m_position[B_site], m_position[next_A_x_site],
+                           m_t1 * edge(B_site, next_A_x_site, s)});
+        m_bonds.push_back({m_position[C_site], m_position[next_A_y_site],
+                           m_t1 * edge(C_site, next_A_y_site, s)});
+        m_bonds.push_back({m_position[B_site], m_position[next_C_x_y_site],
+                           m_t1 * edge(B_site, next_C_x_y_site, s)});
+      }
+    }
+  }
+
+  Expression edge(size_t from_orbital, size_t to_orbital, Operator::Spin spin) {
+    QMUTILS_ASSERT(from_orbital != to_orbital);
+    Expression result;
+    result += Term::Boson::one_body(spin, from_orbital, spin, to_orbital);
+    result -= Term::Boson::one_body(spin, to_orbital, spin, from_orbital);
+    return result;
   }
 
  public:
@@ -172,7 +212,7 @@ int main() {
   const float t1 = 1.0f;
   const float U = 0.5f;
   KagomeModel<Lx, Ly> model(t1, U);
-  HardCoreBosonicBasis basis(Lx * Ly * 3, P);
+  BosonicBasis basis(Lx * Ly * 3, P);
 
   const auto& positions = model.position();
 
@@ -188,19 +228,23 @@ int main() {
     }
   }
 
+  const auto& bonds = model.bonds();
+  std::vector<arma::sp_cx_fmat> bonds_matrices(bonds.size());
+  for (size_t i = 0; i < bonds.size(); i++) {
+    bonds_matrices[i] =
+        compute_matrix_elements<arma::sp_cx_fmat>(basis, bonds[i].op);
+  }
+
   auto H_matrix =
       compute_matrix_elements<arma::sp_cx_fmat>(basis, model.hamiltonian());
 
-  size_t Xoffset = Lx / 2;
-  size_t Yoffset = Ly / 2;
+  size_t Xoffset = Lx / 2 - 1;
+  size_t Yoffset = Ly / 2 - 1;
 
-  auto state = model.v(Xoffset + 0, Yoffset + 0) * model.f(Yoffset + 0);
-  // (model.v(Xoffset - 1, Yoffset + 0) * model.v(Xoffset + 0, Yoffset - 1) +
-  //  model.v(Xoffset, Yoffset + 0) * model.v(Xoffset - 1, Yoffset - 1)) +
-  // (std::sqrt(Lx * Ly) / 6.0f) * model.f(Yoffset + 0) * model.g(Xoffset + 0);
+  auto state =
+      model.v(Xoffset + 0, Yoffset + 0) * model.v(Xoffset + 0, Yoffset + 0);
 
-  auto state_vector =
-      compute_vector_elements_serial<arma::cx_fvec>(basis, state);
+  auto state_vector = compute_vector_elements<arma::cx_fvec>(basis, state);
   state_vector /= arma::norm(state_vector);
 
   float initial_time = 0.0f;
@@ -208,41 +252,60 @@ int main() {
   size_t num_time_steps = 1000;
   TimeIntegrator integrator(initial_time, final_time, num_time_steps, H_matrix);
 
-  std::ofstream outfile("/tmp/kagome_evolution.dat");
-
-  // auto initial_state_vector = state_vector;
+  auto initial_state_vector = state_vector;
+  std::ofstream return_file("/tmp/kagome_return_probability.dat");
+  std::ofstream evolution_file("/tmp/kagome_evolution.dat");
+  std::ofstream current_file("/tmp/kagome_current.dat");
 
   size_t frame = 0;
   do {
     if (frame % 100 == 0) {
       std::cerr << "Frame: " << frame << "\n";
-      std::cerr << "Energy: "
-                << arma::cdot(state_vector, H_matrix * state_vector).real()
-                << "\n";
     }
 
-    // outfile << frame << " "
-    //         << std::norm(arma::cdot(initial_state_vector, state_vector))
-    //         << std::endl;
-    outfile << "\n\n# Frame " << frame << "\n";
-    outfile << "# x y density\n";
+    return_file << frame << " "
+                << std::norm(arma::cdot(initial_state_vector, state_vector))
+                << "\n";
 
-    for (size_t y = 0; y < Ly; y++) {
-      for (size_t x = 0; x < Lx; x++) {
-        for (size_t site = 0; site < 3; site++) {
-          size_t index = model.index().to_orbital(x, y, site);
-          float density =
-              arma::cdot(state_vector,
-                         density_operator_matrices[index] * state_vector)
-                  .real();
-          outfile << positions[index].x << " " << positions[index].y << " "
-                  << density << "\n";
-        }
+    {
+      evolution_file << "\n\n# Frame " << frame << "\n";
+      evolution_file << "# x y density\n";
+
+      for (size_t index = 0; index < positions.size(); ++index) {
+        auto density =
+            arma::dot(arma::conj(state_vector),
+                      density_operator_matrices[index] * state_vector)
+                .real();
+        evolution_file << positions[index].x << " " << positions[index].y << " "
+                       << density << "\n";
       }
     }
+
+    {
+      current_file << "\n\n# Frame " << frame << "\n";
+      current_file << "# x y density\n";
+      for (size_t i = 0; i < bonds.size(); i++) {
+        if (std::abs(bonds[i].final_position.x - bonds[i].initial_position.x) >
+                1.0f ||
+            std::abs(bonds[i].final_position.y - bonds[i].initial_position.y) >
+                1.0f) {
+          continue;
+        }
+        auto current =
+            arma::cdot(state_vector, bonds_matrices[i] * state_vector).imag();
+        current_file
+            << bonds[i].initial_position.x << " " << bonds[i].initial_position.y
+            << " " << (bonds[i].final_position.x - bonds[i].initial_position.x)
+            << " " << (bonds[i].final_position.y - bonds[i].initial_position.y)
+            << " " << current << "\n";
+      }
+    }
+
     frame++;
   } while (integrator.step(state_vector));
 
-  outfile.close();
+  return_file.close();
+  evolution_file.close();
+  current_file.close();
   return 0;
 }
